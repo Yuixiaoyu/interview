a37d65fab5eab95444a56371c58d575192af1622
fy <1004579896@qq.com>
2025-05-20 09:07:11 +0800

    初始化

---
+++ interview-backend/src/main/java/com/xiaoyu/interview/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.xiaoyu.interview.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xiaoyu.interview.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
